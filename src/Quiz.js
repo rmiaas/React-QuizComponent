ddfac8d5e992c3dbc9e6fd43255fc704595abf79
unknownguytoday@gmail.com <34234631+rmiaas@users.noreply.github.com>
2019-04-02 18:03:59 +0800

    Fix creating a QuizEnd component

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,11 +11,12 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = this.state.quiz_position === quizData.quiz_questions.length
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-                {isQuizEnd ? <QuizEnd /> : null}
+                {isQuizEnd ?
+                    <QuizEnd /> :
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
             </div>
         )
     }
